b1aa55330227c2df6317f9450ecf01aed736a8db
Subarna Saikia <saikiasubarna74@gmail.com>
2024-04-08 04:39:57 +0530

    Created home.html template

--- todo_main/settings.py
+++ todo_main/settings.py
@@ -42,7 +42,7 @@ ROOT_URLCONF = 'todo_main.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
